22759ceac9ffc32c8e0b4a3f4873f46b2b6de438
hamzah sm <70360637+Hamzahsm@users.noreply.github.com>
2021-09-26 14:08:41 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+  <title>Tutorial How To Input Checkbox Values Into Mysql</title>
+</head>
+<body>
+  <form action="index.php" method="POST">
+    <label> Choose Your Favori</label>
+  </form>
+</body>
