67aa79fa398f2b5335bc87bdd3db6afbce07803d
hamzah sm <70360637+Hamzahsm@users.noreply.github.com>
2021-09-26 15:29:24 +0700

    Update index.php
    menambahkan form dan membuat pemrosesan data ke database dengan php

--- index.php
+++ index.php
@@ -1,10 +1,43 @@
+<?php 
+//connecting to your database
+$conn = mysqli_connect('localhost', 'username', 'password', 'dbname);
+//checking if connection fail
+if(mysqli_connect_errno()){
+  echo"Failed connect to mysql : ". mysqli_connect_error();
+}
+
+//if submit was clicked 
+if(isset($_POST['submit'])){
+  //the values will separate with comma using  implode function 
+  $fruits = implode(', ', $_POST['fruits'];
+  
+  //query start here 
+  $query = "INSERT INTO yourtabledatabase (fruits) VALUES ('$fruits')";
+  $mysqli_run = myssqli_query($conn, $query);
+
+}
+?>
+
 <!doctype html>
 <html>
 <head>
   <title>Tutorial How To Input Checkbox Values Into Mysql</title>
+  <style>
+    .submit {
+      padding: 10px;
+      border: none;
+      background-color: blue;
+      color: white;
+      box-shadow:5px 5px 5px silver;
+    }
+  </style>
 </head>
 <body>
   <form action="index.php" method="POST">
-    <label> Choose Your Favori</label>
+    <label> Choose Your Favourite Fruits are : </label>
+    <input type="checkbox" name="fruits[]" value="Banana"> Banana <br>
+    <input type="checkbox" name="fruits[]" value="Watermelon"> Watermelon <br>
+    <input type="checkbox" name="fruits[]" value="Orange"> Orange <br>
+    <button type="submit" name="submit" class="submit"> submit</button>
   </form>
 </body>
